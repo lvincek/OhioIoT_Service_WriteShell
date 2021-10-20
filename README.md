09d464936f27f2b316b456cbbf377d9645e606fd
lvincek <larry@vincek.com>
2021-10-20 13:00:13 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# OhioIoT_Service_WriteShell
---
+++ index.js
@@ -0,0 +1,29 @@
+
+
+
+
+
+const { exec } = require('child_process');
+
+
+module.exports = function(command) {
+	return new Promise(function(resolve, reject) {
+		exec(command, (error, stdout, stderr) => {
+			// console.log(`\nexec stdout:\n${stdout}`);
+	        if (error) {
+	        	console.log("GOT AN ERROR IN SHELL EXEC");
+	            console.error(`\nexec error:\n ${error}`);
+	            console.log(`\nexec stderr:\n ${stderr}`);
+	            resolve({error, stderr});
+	        }
+	        resolve({stdout})
+	    });
+
+	})
+	.catch(function(err) {
+		console.log("error in ohioiot writeShell service:");
+		console.log(err);
+	});
+}	
+
+			
