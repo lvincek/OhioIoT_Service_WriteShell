53471bb310e45730299ca9425941ac12ee0a04c5
lvincek <larry@vincek.com>
2023-01-16 12:49:41 -0500

    e

--- app.js
+++ app.js
@@ -1,24 +1,52 @@
 
 
-
-
-require('dotenv').config({});
+require('dotenv').config({path: '../.env'});
 
 const express = require('express');
 const app = express();
 
-const db_functions = require('./definitions');
-const responses = require('./responses')(db_functions);
-const setRoutes = require('./routes');
-
-// console.log("\t.. our db_functions:");
-// console.log(db_functions);
+const shell = require('./shell');
 
 
-setRoutes(app, responses);
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+app.get('/', function(req, res, next) {
+
+	console.log("got it");
+	res.send("got it");
+
+})
+
+app.post("/", function(req, res, next) {
+
+	console.log("incoming:", req.body);
+
+	if (	
+		!req.body || 	
+		!req.body.hasOwnProperty('msg') ||
+		typeof req.body.msg != 'string'
+	) {
+		console.log("invalid payload - returning");
+		next();
+	} 
+
+	return shell(req.body.msg)
+	.then(function(result) {
+		console.log("response:", result);
+		res.json(result);
+	})
+
+})
+
+app.use(function(req, res, next, error) {
+	console.log("failed to clear the route:");
+	console.log(error);
+})
 
 
-let port = process.env.MODELS_PORT ? process.env.MODELS_PORT : 2222;
+
+let port = process.env.SHELL_PORT ? process.env.SHELL_PORT : 5555;
 
 app.listen(port, (err) => {
 	if (err) { console.error("got an error opening port "+port); }
--- shell.js
+++ shell.js
@@ -1,8 +1,6 @@
 
 
 
-
-
 const { exec } = require('child_process');
 
 
